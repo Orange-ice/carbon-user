5b73b1b808d44ac1b1878357f7f48ad7b15c3237
huangb <huangbing@ecdigit.com>
2024-03-26 15:58:34 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="title flex">
+    <div class="mr-10 font-size-18">hello carbon</div>
+    <div>welcome</div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.title {
+  color: red;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'the-new-css-reset/css/reset.css';
+import 'virtual:uno.css';
+
+createApp(App).mount('#app');
